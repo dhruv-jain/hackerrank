04d288ad4efb330d025afe7ddbf5c0bfdbbef9e3
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-14 21:00:36 -0400

    Create loop.cpp

---
+++ cpp/loop.cpp
@@ -0,0 +1,43 @@
+/*Given an integer, , print its first  multiples. 
+Each multiple  (where ) should be printed on a new line in the form: n x i = result.
+*/
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <= 10; i++) {
+        
+        int result = i*n;
+        cout << n << " x " << i << " = " << result << endl;
+    }
+    
+    
+    return 0;
+}
