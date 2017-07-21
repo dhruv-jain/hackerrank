8e7f8eb37fac0152bba5374dd963b9b2b9f4edba
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-21 19:19:16 -0400

    Create fibonacci_recursive.cpp

---
+++ cpp/fibonacci_recursive.cpp
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+#include <iostream>
+
+using namespace std;
+
+int fibonacci(int n) {
+    // Complete the function.
+    if(n==0){return 0;}
+    if(n==1){return 1;}
+    else return (fibonacci(n-1) + fibonacci(n-2));
+    
+
+}
+int main() {
+    int n;
+    cin >> n;
+    cout << fibonacci(n);
+    return 0;
+}
