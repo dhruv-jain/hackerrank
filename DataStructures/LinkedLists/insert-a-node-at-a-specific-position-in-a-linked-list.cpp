18473828c2cdc923e49f21accc5045c077681967
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-24 23:08:31 -0400

    Update insert-a-node-at-a-specific-position-in-a-linked-list.cpp

--- DataStructures/LinkedLists/insert-a-node-at-a-specific-position-in-a-linked-list.cpp
+++ DataStructures/LinkedLists/insert-a-node-at-a-specific-position-in-a-linked-list.cpp
@@ -1,1 +1,39 @@
 //https://www.hackerrank.com/challenges/insert-a-node-at-a-specific-position-in-a-linked-list/
+/*
+  Insert Node at a given position in a linked list 
+  head can be NULL 
+  First element in the linked list is at position 0
+  Node is defined as 
+  struct Node
+  {
+     int data;
+     struct Node *next;
+  }
+*/
+Node* InsertNth(Node *head, int data, int position)
+{
+    Node *newNode = new Node;
+    newNode->data = data;
+    if(head==nullptr){
+        return newNode;
+    }
+    else {
+        Node *tmp =head;
+        int p=0;
+        if(position == 0) {
+            newNode->next = head;
+            return newNode;
+        }
+        while(tmp!=nullptr){
+            if(p == position-1) {
+                newNode->next = tmp->next;
+                tmp->next = newNode;
+                } else {
+                tmp = tmp->next;
+            }
+           p++;
+        } 
+    }
+                return head;
+
+}
