0c10b0fe504b2b661a87147cb2f426599f78cd31
Dhruv Jain <dhruvkc16@gmail.com>
2017-08-19 13:28:53 -0400

    Create amazon_user_bandwidth.cpp

---
+++ amazon_user_bandwidth.cpp
@@ -0,0 +1,10 @@
+/*1. You are given a log file with the bandwidth information of users in several lines. 
+Bandwidth info for a certain user may be repeated.
+
+User:A Bandwidth:50 CountryCode:IND
+User:B Bandwidth:60 CountryCode:USA
+User:A Bandwidth:70 CountryCode:IND
+
+(i) Find total amount of bandwidth consumed per user
+(ii) Suppose there are 4 country codes, for every country code, find the top 5 users which consumed maximum bandwidth sorted.
+*/
